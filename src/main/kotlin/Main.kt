0a442c563d8902be81cac20ff107415ac49d5f10
XZTDean <xztdean@gmail.com>
2024-01-20 14:29:54 -0800

    Add a try catch to main to avoid program crash

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,20 @@ fun main() {
     val ipChecker = IpChecker()
     val updater = DdnsUpdater()
     while (true) {
-        val ip = ipChecker.getIp()
-        if (ip.isNotBlank()) {
-            updater.updateDns(ip)
+        try {
+            val ip = ipChecker.getIp()
+            if (ip.isNotBlank()) {
+                updater.updateDns(ip)
+            }
+        } catch (e: Exception) {
+            if (e is InterruptedException) {
+                throw e
+            }
+            Logger.log("Exception: $e")
+            e.stackTrace.forEach {
+                Logger.log("\t" + it.toString())
+            }
+            Logger.saveLog()
         }
         Thread.sleep(1000*60*5) // 5 minutes
     }
